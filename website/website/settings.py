d525f721a8bc2d1735d2f2fb3fbf6acc11231ef0
AbduboriyKhodjimatov <abduboriykhodjimatov1607@gmail.com>
2024-05-28 19:52:23 +0500

    added allowed hosts

--- website/website/settings.py
+++ website/website/settings.py
@@ -24,7 +24,10 @@ SECRET_KEY = 'django-insecure-e4(8wz^3qo#hqy)g9-v1!o5$b-z*vvwq9*3a*gfp6_y9)+y77g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'akhj.pythonanywhere.com',
+    '127.0.0.1'
+]
 
 # Application definition
 
